3680b97dde532b686c3dbd4b2397aac0e0ababba
Student <Student@A-KOMPUTER>
2016-10-10 13:18:56 +0200

    rownanie kwadratowe

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+	int c, b, a = c = b = 3;
+	cin >> a >> b >> c;
+	int delta = b*b - 4 * a*c;
+	if (delta < 0)
+		cout << "no results !\n";
+	else {
+		cout <<"0="
+	}
+
+	std::cout << "a+b";
+	return 0;
+}
---
+++ ConsoleApplication1/enc_temp_folder/23f117ce5c9a417922f1fe1ea6f61e7f/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a = 3, b = 5;
+	std::cout << "a+b";
+	return 0;
+}
